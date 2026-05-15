8aba4997a430a3332cc505e173ed83018091f465
Zsolt 1988 <finterest88@gmail.com>
2026-05-15 08:55:43 +0200

    Automatisches Portfolio Update

--- wix-wrapper.js
+++ wix-wrapper.js
@@ -9,7 +9,7 @@ class AutoResizingIframe extends HTMLElement {
         this.innerHTML = `
             <iframe 
                 src="https://zsolt1988.github.io/new-era-mobility/" 
-                style="width: 100%; height: 100%; border: none; overflow: hidden; display: block;" 
+                style="width: 100%; height: 100%; border: none; overflow: hidden; display: block; margin: 0; padding: 0;" 
                 scrolling="no">
             </iframe>
         `;
